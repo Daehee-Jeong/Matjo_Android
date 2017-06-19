a8107c249a31c213e401fc277737c8344f75395a
kosta-PC\kosta <y229600@naver.com>
2017-06-19 16:19:00 +0900

    민주 : 모임 상세정보 - 리뷰 부분 다 됨 : 이미지 띄워주는 부분과 레이아웃 수정이 필요함

--- Matjo/app/src/main/java/com/kosta148/matjo/view/GroupReviewFragment.java
+++ Matjo/app/src/main/java/com/kosta148/matjo/view/GroupReviewFragment.java
@@ -10,8 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.kosta148.matjo.R;
 import com.kosta148.matjo.adapter.ExpandableListAdapter;
+import com.kosta148.matjo.bean.PereviewBean;
 import com.kosta148.matjo.bean.ReviewBean;
 
 import java.util.ArrayList;
@@ -31,6 +34,11 @@ public class GroupReviewFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_restaurant_review, container, false);
 
         ArrayList<ReviewBean> reviewList = getArguments().getParcelableArrayList("reviewList");
+        Gson gson = new Gson();
+        for (int i = 0; i < reviewList.size(); i++) {
+            ArrayList<PereviewBean> pereviewBeanList = gson.fromJson(reviewList.get(i).getPereviewJSArray(), new TypeToken<ArrayList<PereviewBean>>() {}.getType());
+            reviewList.get(i).setPereviewList(pereviewBeanList);
+        }
 
         groupDetailActivity = (GroupDetailActivity) getActivity();
 
@@ -39,21 +47,16 @@ public class GroupReviewFragment extends Fragment {
         recyclerview.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
         List<ExpandableListAdapter.Item> data = new ArrayList<>();
 
-        ExpandableListAdapter.Item places = new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, "사람A", R.drawable.profile, 3.5);
-        places.invisibleChildren = new ArrayList<>();
-        places.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, getResources().getString(R.string.long_text), R.drawable.img06));
-        data.add(places);
-
         for (int i = 0; i < reviewList.size(); i++) {
             // TODO 업소 이미지 받아올 수 있나요?
             ExpandableListAdapter.Item placeTmp = new ExpandableListAdapter.Item(ExpandableListAdapter.HEADER, reviewList.get(i).getReviewRestaName(), R.drawable.profile, Double.valueOf(reviewList.get(i).getAvgRating()));
             placeTmp.invisibleChildren = new ArrayList<>();
-//            List<PereviewBean> perTmpList = reviewList.get(i).getPereviewList();
-//            int perSize = perTmpList.size();
-//            for (int j = 0; j < perSize; j++) {
-//                PereviewBean perTmp = perTmpList.get(j);
-//                placeTmp.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, perTmp.getPereviewMemName()+":"+perTmp.getPereviewContent(), R.drawable.img06));
-//            }
+            List<PereviewBean> perTmpList = reviewList.get(i).getPereviewList();
+            int perSize = perTmpList.size();
+            for (int j = 0; j < perSize; j++) {
+                PereviewBean perTmp = perTmpList.get(j);
+                placeTmp.invisibleChildren.add(new ExpandableListAdapter.Item(ExpandableListAdapter.CHILD, perTmp.getPereviewMemName()+":"+perTmp.getPereviewContent(), R.drawable.img06));
+            }
             data.add(placeTmp);
         }
 
