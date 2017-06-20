8bc4e6447b9e6d994f51b855494ff63dfa99dbf5
kosta-PC\kosta <y229600@naver.com>
2017-06-20 15:42:59 +0900

    민주 : 앱 뉴스피드 리스트뷰 구현 - 스크롤 맨 아래로 가면 페이징 처리!

---
+++ Matjo/app/src/main/java/com/kosta148/matjo/adapter/NewsFeedAdapter.java
@@ -0,0 +1,90 @@
+package com.kosta148.matjo.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.kosta148.matjo.R;
+import com.kosta148.matjo.bean.NewsFeedBean;
+
+import java.util.List;
+
+/**
+ * Created by Daehee on 2017-06-17.
+ */
+
+public class NewsFeedAdapter extends BaseAdapter {
+    Context context;
+    int layout;
+    List<NewsFeedBean> newsFeedList;
+    LayoutInflater inflater;
+
+    public NewsFeedAdapter(Context context, int layout, List<NewsFeedBean> restaList) {
+        this.context = context;
+        this.layout = layout;
+        this.newsFeedList = restaList;
+        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    } // Constructor
+
+    @Override
+    public int getCount() {
+        return newsFeedList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return newsFeedList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_resta_list, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.ivNewsImg = (ImageView)convertView.findViewById(R.id.ivRestaImg);
+            viewHolder.tvNewsTitle = (TextView)convertView.findViewById(R.id.tvRestaTitle);
+            viewHolder.tvNewsCate = (TextView)convertView.findViewById(R.id.tvRestaCate);
+            viewHolder.tvNewsAddr = (TextView)convertView.findViewById(R.id.tvRestaAddr);
+            convertView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        NewsFeedBean nBean = (NewsFeedBean) getItem(position);
+        String type = nBean.getType();
+        if (type.equals("1")) { //모임
+            Glide.with(context).load("http://ldh66210.cafe24.com/upload/ic_group.png").thumbnail(0.1f).error(R.mipmap.ic_launcher).into(viewHolder.ivNewsImg);
+            viewHolder.tvNewsTitle.setText("["+nBean.getGroupName()+"] 모임이 등록되었습니다");
+            viewHolder.tvNewsCate.setText(nBean.getRegDate());
+            viewHolder.tvNewsAddr.setText("모임장 | "+nBean.getGroupLeader());
+        } else { // 리뷰 등록
+            Glide.with(context).load("http://ldh66210.cafe24.com/upload/ic_resta.png").thumbnail(0.1f).error(R.mipmap.ic_launcher).into(viewHolder.ivNewsImg);
+            viewHolder.tvNewsTitle.setText("["+nBean.getRestaName()+"]의 리뷰가 등록되었습니다");
+            viewHolder.tvNewsCate.setText(nBean.getRegDate());
+            viewHolder.tvNewsAddr.setText(nBean.getRestaCate());
+        }
+        return convertView;
+    }
+
+    static class ViewHolder {
+        public ImageView ivNewsImg;
+        public TextView tvNewsTitle;
+        public TextView tvNewsCate;
+        public TextView tvNewsAddr;
+    } // end of inner class ViewHolder
+} // end of class
--- Matjo/app/src/main/java/com/kosta148/matjo/view/NewsFeedFragment.java
+++ Matjo/app/src/main/java/com/kosta148/matjo/view/NewsFeedFragment.java
@@ -1,25 +1,160 @@
 package com.kosta148.matjo.view;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 import com.kosta148.matjo.R;
+import com.kosta148.matjo.adapter.NewsFeedAdapter;
+import com.kosta148.matjo.bean.NewsFeedBean;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class NewsFeedFragment extends Fragment {
     MainActivity mainActivity;
+    ListView listView;
+    List<NewsFeedBean> newsFeedList = new ArrayList<NewsFeedBean>();
+    NewsFeedAdapter newsFeedAdapter;
+    Handler handler = new Handler();
 
+    int pageNo = 1;  // 초기값
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v;
         v = inflater.inflate(R.layout.fragment_news_feed, container, false);
         mainActivity = (MainActivity) getActivity();
 
+        pageNo = 1;
+        // 초기 리스트
+        callNewsFeedList(pageNo+"");
+
+        listView = (ListView) v.findViewById(R.id.listView);
+        listView.setOnScrollListener(mOnScrollListener);
+        listView.setOnItemClickListener(mOnItemClickListener);
+        newsFeedAdapter = new NewsFeedAdapter(getActivity().getApplicationContext(),
+                R.layout.item_resta_list,
+                newsFeedList);
+        listView.setAdapter(newsFeedAdapter);
+
         return v;
     } // end of onCreateView
+
+    AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            mainActivity.showToast(newsFeedList.get(position).getTypeMsg());
+            NewsFeedBean nBean = newsFeedList.get(position);
+
+            //TODO : type에 따라 다른 컨트롤러 호출
+
+        }
+    }; // end of ItemClickListener
+
+    boolean lastItemVisibleFlag = false;        //화면에 리스트의 마지막 아이템이 보여지는지 체크
+    AbsListView.OnScrollListener mOnScrollListener = new AbsListView.OnScrollListener() {
+        boolean lastitemVisibleFlag = false; // 마지막이냐 아니냐의 플래그
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+            //현재 화면에 보이는 첫번째 리스트 아이템의 번호(firstVisibleItem) + 현재 화면에 보이는 리스트 아이템의 갯수(visibleItemCount)가 리스트 전체의 갯수(totalItemCount) -1 보다 크거나 같을때
+            lastItemVisibleFlag = (totalItemCount > 0) && (firstVisibleItem + visibleItemCount >= totalItemCount);
+        }
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+            //OnScrollListener.SCROLL_STATE_IDLE은 스크롤이 이동하다가 멈추었을때 발생되는 스크롤 상태
+            //즉 스크롤이 바닦에 닿아 멈춘 상태에 처리를 하겠다는 뜻
+            if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE && lastItemVisibleFlag) { // '리스트의 마지막에' + '멈췄다'
+                callNewsFeedList(++pageNo+""); // 데이터 추가 요청
+            }
+        }
+    }; // end of OnScrollListener
+
+    void callNewsFeedList(String pageNoParam) {
+        final String pageNo = pageNoParam;
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://ldh66210.cafe24.com/newsfeed/selectNewsFeedProc.do"
+                , new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.d("MyLog", "response : " + response);
+                final String res = response;
+                // JSON 1차 파싱
+                JsonObject root = new JsonParser().parse(res).getAsJsonObject();
+                // 뉴스피드 목록 Json
+                JsonArray newsFeedListJSArray = root.get("newsFeedList").getAsJsonArray();
+                Gson gson = new Gson();
+
+                // 뉴스피드 목록
+                final ArrayList<NewsFeedBean> newsFeedListTmp = gson.fromJson(newsFeedListJSArray.toString(), new TypeToken<ArrayList<NewsFeedBean>>() {}.getType());
+
+                Log.d("MyLog", "send Start");
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 성공 시 값 설정
+                        for (int i = 0; i < newsFeedListTmp.size() ; i++) {
+                            newsFeedList.add(newsFeedListTmp.get(i));
+                        }
+                        newsFeedAdapter.notifyDataSetChanged();
+                    }
+                });
+                Log.e("MyLog", "send Finish");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("MyLog", "error : " + error);
+                final VolleyError err = error;
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity().getApplicationContext(), "error : " + err, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("pageNo",pageNo+"");
+                return params;
+            }
+        };
+        // 재 호출 설정
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(
+                DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 5,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        requestQueue.add(stringRequest);
+    }
+
 } // end of class
 
