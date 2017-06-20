1dd2fab73973732ff3417d7c6f86fe55dcc4fb62
ldh66210 <ldh66210@gmail.com>
2017-06-21 00:16:31 +0900

    1. 스플래쉬 화면에서 자동로그인 바로 가능

--- Matjo/app/src/main/java/com/kosta148/matjo/view/SplashActivity.java
+++ Matjo/app/src/main/java/com/kosta148/matjo/view/SplashActivity.java
@@ -2,6 +2,7 @@ package com.kosta148.matjo.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -12,12 +13,38 @@ import com.kosta148.matjo.R;
  */
 
 public class SplashActivity extends Activity {
+
+    // SharedPreferences 선언
+    private SharedPreferences sharedPreferences;
+    // SharedPreferences 키 상수
+    private static final String SHAREDPREFERENCES_LOGIN_AUTO = "AutoLogin";
+
     Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // SharedPreferences 초기화
+        sharedPreferences = getSharedPreferences("LoginSetting.dat", MODE_PRIVATE);
+    } // end of onCreate
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        if (sharedPreferences != null) {
+            if (sharedPreferences.getBoolean(SHAREDPREFERENCES_LOGIN_AUTO, false)) {
+                // 자동 로그인 설정시
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -26,11 +53,6 @@ public class SplashActivity extends Activity {
                 finish();
             }
         }, 1000);
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();
-    }
-}
+        super.onResume();
+    } // end of onResume
+} // end of class
